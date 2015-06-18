a2125a95718e62025c2c704004c1539c186469fe
Marlon Cepeda <warlhunters@gmail.com>
2015-06-18 18:52:53 -0430

    [MOD] Modificado archivo subscriberController para agregar la funcion publica create

--- apps/frontend/controllers/SubscribersController.php
+++ apps/frontend/controllers/SubscribersController.php
@@ -2,13 +2,39 @@
 
 namespace Sirehu\Frontend\Controllers;
 
+use Sirehu\Frontend\Models\WebSubscribers;
+
 class SubscribersController extends ControllerBase
 {
-	
+
     public function indexAction()
     {
 
     }
 
+    public function createAction()
+    {
+     
+        $subscriber = new WebSubscribers();
+        $subscriber->setCorreo($this->request->getPost("correo"));
+        $subscriber->setFechaCreacion(date("d-m-Y"));
+        
+        
+
+        if (!$subscriber->save()) {
+            foreach ($subscriber->getMessages() as $message) {
+                $this->flash->error($message);
+            }
+            $this->flash->success("La Solicitud no ha sido creada satifactoriamente");
+        }else{
+
+            $this->flash->success("La Solicitud ha sido creada satifactoriamente");
+
+            return $this->dispatcher->forward(array(
+                "controller" => "subscribers",
+                "action" => "index"
+            ));
+        }
+    }
 }
 
