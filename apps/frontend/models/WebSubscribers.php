650dfc266dac0296912d1d9cfdb3fb890073297e
Marlon Cepeda <warlhunters@gmail.com>
2015-06-18 18:18:34 -0430

    [ADD] agregada nueva tabla webSubscribers al modulo frontend

---
+++ apps/frontend/models/WebSubscribers.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Sirehu\Frontend\Models;
+
+class WebSubscribers extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     *
+     * @var string
+     */
+    protected $correo;
+
+    /**
+     *
+     * @var string
+     */
+    protected $fecha_creacion;
+
+    /**
+     * Method to set the value of field id
+     *
+     * @param integer $id
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field correo
+     *
+     * @param string $correo
+     * @return $this
+     */
+    public function setCorreo($correo)
+    {
+        $this->correo = $correo;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field fecha_creacion
+     *
+     * @param string $fecha_creacion
+     * @return $this
+     */
+    public function setFechaCreacion($fecha_creacion)
+    {
+        $this->fecha_creacion = $fecha_creacion;
+
+        return $this;
+    }
+
+    /**
+     * Returns the value of field id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Returns the value of field correo
+     *
+     * @return string
+     */
+    public function getCorreo()
+    {
+        return $this->correo;
+    }
+
+    /**
+     * Returns the value of field fecha_creacion
+     *
+     * @return string
+     */
+    public function getFechaCreacion()
+    {
+        return $this->fecha_creacion;
+    }
+
+    public function getSource()
+    {
+        return 'web_subscribers';
+    }
+
+    /**
+     * Independent Column Mapping.
+     */
+    public function columnMap()
+    {
+        return array(
+            'id' => 'id', 
+            'correo' => 'correo', 
+            'fecha_creacion' => 'fecha_creacion'
+        );
+    }
+
+}
